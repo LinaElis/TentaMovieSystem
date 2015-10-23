3b259306540eb7bf3289b449ef6b05f423d999ec
Lina Wellerstrand <lina.wellerstrand@gmail.com>
2015-10-23 11:25:41 +0200

    First commit

---
+++ MovieSystemTests/TestNetflix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieSystemTests
+{
+    //Testerna är ofärdig = osäker på hur jag skulle skriva dem..
+
+    [TestClass]
+    public class TestNetflix
+    {
+
+        [TestMethod]
+        public void Test_To_Create_Facade_Netflix()
+        {
+            IPlayer player = PlayerFactory.CreatePlayer(movieName);
+
+            MovieFacade sut = new MovieFacade(movieName);
+
+            sut.Play("Star Wars 3");
+
+            Assert.IsTrue(sut.HasBeenPlayed);
+
+        }
+    }
+}
---
+++ MovieSystemTests/TestSF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieSystemTests
+{
+    //Testerna är ofärdig = osäker på hur jag skulle skriva dem..
+
+    [TestClass]
+    public class TestSF
+    {
+        [TestMethod]
+        public void Test_To_Create_Facade_SF()
+        {
+            MovieFacade facade = new MovieFacade();
+
+            Assert.IsFalse(facade.HasBeenPlayed);
+        }
+    }
+}
+
